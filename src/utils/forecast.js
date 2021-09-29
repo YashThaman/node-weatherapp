0ad7222d1492b85eba9fc06554745bde6f3c6257
YashThaman <yash.thaman@gmail.com>
2021-09-29 11:13:00 +0530

    New feature of  humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (longitude , latitude ,callback)=>{
             callback('Unable to find location. Try again!', undefined)
         }else{
             callback(undefined,
-                body.current.weather_descriptions[0]+' . It is currently '+body.current.temperature+' degree out. It feels like '+body.current.feelslike+' degree out .'
+                body.current.weather_descriptions[0]+' . It is currently '+body.current.temperature+' degree out. It feels like '+body.current.feelslike+' degree out . The humidity is :'+body.current.humidity+"%"
  
                 )
         }
